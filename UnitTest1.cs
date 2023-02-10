22570563ee181a3a6c1d6fe1e0641050fbc11fa2
Samuel K D L Davidson <123samueld@gmail.com>
2023-02-10 15:30:16 +0000

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using MindboxCodeTask_GeometryCalc;
+
+namespace TestMindboxCalculatorTask
+{
+    public class AreaCalcTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
